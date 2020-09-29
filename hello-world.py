21667dfbbd7ae4e20bc243c1524d425314b166fe
Justin Wagg <9845115+justinwagg@users.noreply.github.com>
2020-09-28 23:33:15 -0400

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,33 @@
 
+from datetime import timedelta
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.utils.dates import days_ago
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': days_ago(2),
+    'retries': 0,
+}
+
+dag = DAG(
+    'hello-world',
+    default_args=default_args,
+    schedule_interval=None,
+    catchup=False,
+)
+
+say_hey = BashOperator(
+    task_id='say_hey',
+    bash_command='echo "Hello World"',
+    dag=dag,
+)
+
+happy_day = BashOperator(
+    task_id='happy_day',
+    bash_command='echo "Happy $(date +"%A")!"',
+    dag=dag,
+)
+
+say_hey >> happy_day
